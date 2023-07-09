5a4550632293a63295b278b2cb83d3da553c9d28
Madman10K <stanv.contact176@gmail.com>
2023-07-09 18:24:30 +0300

    Update to not require struct in C and fix namespace

--- C/cuexec.cpp
+++ C/cuexec.cpp
@@ -1,4 +1,4 @@
-struct RunnerData;
+typedef struct uexec_RunnerData uexec_RunnerData;
 
 #include "cuexec.h"
 #include "../uexec.hpp"
@@ -13,47 +13,47 @@ extern "C"
         return uexec::execandwait(command);
     }
 
-    int uexec_runner_init(struct RunnerData* data, char* const* args, bool bOpenStderrPipe, bool bOpenStdoutPipe, bool bOpenStdinPipe)
+    int uexec_runner_init(uexec_RunnerData* data, char* const* args, bool bOpenStderrPipe, bool bOpenStdoutPipe, bool bOpenStdinPipe)
     {
         return cast(data)->init(args, bOpenStderrPipe, bOpenStdoutPipe, bOpenStdinPipe);
     }
 
-    void uexec_runner_update(struct RunnerData* data)
+    void uexec_runner_update(uexec_RunnerData* data)
     {
         cast(data)->update();
     }
 
-    void uexec_runner_destroy(struct RunnerData* data)
+    void uexec_runner_destroy(uexec_RunnerData* data)
     {
         cast(data)->destroy();
     }
 
-    bool uexec_runner_valid(struct RunnerData* data)
+    bool uexec_runner_valid(uexec_RunnerData* data)
     {
         return cast(data)->valid();
     }
 
-    void uexec_runner_destroyForReuse(struct RunnerData* data)
+    void uexec_runner_destroyForReuse(uexec_RunnerData* data)
     {
         cast(data)->destroyForReuse();
     }
 
-    bool uexec_runner_finished(struct RunnerData* data)
+    bool uexec_runner_finished(uexec_RunnerData* data)
     {
         return cast(data)->finished();
     }
 
-    bool uexec_runner_startable(struct RunnerData* data)
+    bool uexec_runner_startable(uexec_RunnerData* data)
     {
         return cast(data)->startable();
     }
 
-    void uexec_runner_terminate(struct RunnerData* data)
+    void uexec_runner_terminate(uexec_RunnerData* data)
     {
         cast(data)->terminate();
     }
 
-    bool uexec_runner_readSTDOUT(struct RunnerData* data, char* buffer, size_t size, size_t* bytesRead)
+    bool uexec_runner_readSTDOUT(uexec_RunnerData* data, char* buffer, size_t size, size_t* bytesRead)
     {
         uexecstring str;
         str.resize(size);
@@ -66,7 +66,7 @@ extern "C"
         return result;
     }
 
-    bool uexec_runner_readSTDERR(struct RunnerData* data, char* buffer, size_t size, size_t* bytesRead)
+    bool uexec_runner_readSTDERR(uexec_RunnerData* data, char* buffer, size_t size, size_t* bytesRead)
     {
         uexecstring str;
         str.resize(size);
@@ -79,7 +79,7 @@ extern "C"
         return result;
     }
 
-    bool uexec_runner_write(struct RunnerData* data, const char* buffer, size_t size, size_t* bytesWritten)
+    bool uexec_runner_write(uexec_RunnerData* data, const char* buffer, size_t size, size_t* bytesWritten)
     {
         uexecstring str = buffer;
         return cast(data)->write(str, size, *bytesWritten);
