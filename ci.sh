ffe4a73d3cc075719c3e5c09bfbbccb7b83e8d6a
Stanislav Vasilev <stanv.contact176@gmail.com>
2023-08-13 00:49:51 +0300

    Create ci.sh

---
+++ ci.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cpus=$(grep -c processor /proc/cpuinfo)
+
+mkdir build
+cd build || exit
+cmake .. -DCMAKE_BUILD_TYPE=RELEASE || exit
+MSBuild.exe uexec.sln -property:Configuration=Release -property:Platform=x64 -property:maxCpuCount="${cpus}" || make -j "${cpus}" || exit
