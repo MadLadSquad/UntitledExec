6bf3f2bce28a78ab25e063a0834f3dbc2969690b
Stanislav Vasilev <stanv.contact176@gmail.com>
2026-06-09 04:41:32 +0300

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -4,4 +4,4 @@ cpus=$(grep -c processor /proc/cpuinfo) || cpus=$(sysctl -n hw.ncpu)
 mkdir build
 cd build || exit
 cmake .. -DCMAKE_BUILD_TYPE=RELEASE || exit
-MSBuild.exe uexec.sln -property:Configuration=Release -property:Platform=x64 -property:maxCpuCount="${cpus}" || make -j "${cpus}" || exit
+MSBuild.exe uexec.sln* -property:Configuration=Release -property:Platform=x64 -property:maxCpuCount="${cpus}" || make -j "${cpus}" || exit
